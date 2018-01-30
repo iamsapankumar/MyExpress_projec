a43f9d7fcbe9656a9c88fe7dca8730298f7996be
Sapan Kumar Das <Sapan Kumar Das>
2018-01-30 22:21:46 +0530

    initial commit add config folder Seperatly

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express =  require('express');
 const bodyparser = require('body-parser');
-
+const morgan = require('morgan')
+const config = require('./config/config')
 const app = express();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}))
+app.use(morgan('combined'))
 
 
 app.get('/student', (req, res)=>{
   res.status(200).send({
-      id: '01',
-      name: "Sapan Kumar Das"
+      id: '011',
+      name: 'Sapan Kumar Das'
   })
 })
 
-const port = process.env.PORT ||3000;
-app.listen(port, ()=>{
-    console.log(`Server runing on port : ${port}`)
+//const port = process.env.PORT ||3000;
+app.listen(config.port, ()=>{
+    console.log(`Server runing on port : ${config.port}`)
 })
 
